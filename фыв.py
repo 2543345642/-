1947e561cbecfc36b7a40af9533cc883fb229995
2543345642 <58842567+2543345642@users.noreply.github.com>
2022-09-13 17:46:59 +0300

    Add files via upload

--- фыв.py
+++ фыв.py
@@ -1,2 +1,19 @@
-faren = int(input("введите вес в фунтах "))
-print("у вас", round((faren - 32) / 1.8, 2) , "киллограмов")
+import random
+user_turn = input("введите камень(1) ножницы(2) и бумага(3)")
+computer_turn= random.randint(1, 3)
+print("игрок показал", user_turn)
+print("игрок показал", computer_turn)
+if user_turn == computer_turn:
+    print("ничья!")
+elif user_turn == 1 and computer_turn == 2:
+    print("игрок победил")
+elif user_turn == 1 and computer_turn == 3:
+    print("пк выиграл")
+elif user_turn == 2 and computer_turn == 1:
+    print("пк выиграл")
+elif user_turn == 2 and computer_turn == 2:
+    print("игрок выиграл")
+elif user_turn == 3 and computer_turn == 2:
+    print("игрок выиграл")
+elif user_turn == 3 and computer_turn == 1:
+    print("игрок выиграл")
