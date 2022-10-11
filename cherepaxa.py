73c184054dd899fcc4fc9cd00c0b0ef5d4c9d807
2543345642 <58842567+2543345642@users.noreply.github.com>
2022-10-11 17:45:56 +0300

    Add files via upload

---
+++ cherepaxa.py
@@ -0,0 +1,41 @@
+import turtle
+turtle.speed(-1)
+da = 0
+turtle.shape("turtle")
+
+while da < 6:
+	turtle.forward(100)
+	turtle.left(90)
+	da += 1
+da = 0
+while da < 4:
+	turtle.forward(100)
+	turtle.right(120)
+	da += 1
+turtle.left(120)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(40)
+turtle.left(90)
+da = 0
+while da < 4:
+	turtle.forward(30)
+	turtle.right(90)
+	da += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+turtle.done()
---
+++ test_cherepaxa_thikl.py
@@ -0,0 +1,64 @@
+import turtle as t
+t.speed()
+t.shape("turtle")
+walls_width = 200
+walls_height = 100
+roof_height = 150
+
+for i in range(4):
+	t.fd(walls_width)
+	t.right(90)
+t.left(50)
+for i in range(2):
+	t.fd(walls_width)
+	t.right(115)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+turtle.done()
