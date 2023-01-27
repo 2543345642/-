051e0f7b9b3c214f6b37d6e14d639f65015d33af
2543345642 <58842567+2543345642@users.noreply.github.com>
2023-01-27 11:37:44 +0300

    Add files via upload

---
+++ dsa.py
@@ -0,0 +1,19 @@
+
+import random
+while True:
+    a = input("введите свой месяц рождения ")
+    b = ["Трахтрахач", "Ванила ass", "dungeon master", "Я съел деда", "Star finger", "star platimum ass", "Oingo Boingo", "Renatal unded immortal van der ded inside", "Sex pistols", "King krimson", "D4C"]
+    if a == "Сентябрь, октябрь, ноябрь":
+        
+        
+    elif a == "Влад" or "Ренатал" or "Тимофей" or "Zimtavort":
+        b = " гигачад"
+        a += b
+        print(a)
+    else:
+        b = " норм чел"
+        a += b
+        print(a)
+    os.system("cls")
+    
+          
