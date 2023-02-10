1cb4872bfe214d5fc247d8567372da98b69c0e1e
2543345642 <58842567+2543345642@users.noreply.github.com>
2023-02-10 16:36:55 +0300

    Add files via upload

---
+++ Library.py
@@ -0,0 +1,114 @@
+import os
+LIBRARY = [
+
+    {
+        "название": "Тестовая книга",
+        "автор": "Каыфавф",
+        "год": 1232
+    }
+
+]
+
+
+def add_book():
+    print("Добавляем книгу ")
+    title = input("Введите название произведения:")
+    if not title:
+        print("нет названия!!! Введите ещё раз название")
+        return
+
+    author = input("Введите имя автора")
+    if not author:
+        print("нет имя автора!!! Введите ещё раз имя автора")
+        return
+
+    year = input("Введите год издания:")
+    if not year:
+        print("нет года!!! Введите ещё раз год")
+        return
+    if year.isdigit():
+        year = int(year)
+    else:
+        print("Год должен быть числом!!! Введите ещё раз год")
+        return
+    book = {"название": title,
+            "автор": author,
+            "год": year,
+    }
+
+    LIBRARY.append(book)
+    print(f"Книга: {title}; автор: {author}; год: {year}, успешно добавлена в библеотеку")
+
+
+def show_library():
+    for num, book in enumerate(LIBRARY, 1):
+        print("номер на полке", num)
+        print("название", book["название"])
+        print("автор", book["автор"])
+        print("год", book["год"])
+
+
+def delete_book() -> None:
+    """
+    Удаляет книгу или выводит сообщение, что нету такой книги.
+
+
+    """
+    #нету книг
+    if not LIBRARY:
+        print("В библеотеке нету книг!!!")
+        return
+    book_number = input("Введите номер(ЦЫФРУ) книги, которую нужно удалить")
+
+    #нету чисел
+    if not book_number.isdigit():
+        print("Ошибка!Номер книги должен быть числом")
+        return
+    book_number = int(book_number)
+    if book_number <= 0:
+        print("Ошибка! Номер должен быть больше 0")
+        return
+    if book_number > len(LIBRARY):
+        print("Ошибка! Номер СЛИШКОМ большой")
+    idx = book_number - 1
+    book_name = LIBRARY[idx]["название"]
+    print("Книга успешна удалена", book_name)
+    LIBRARY.pop(book_number - 1)
+
+
+def search_by_number() -> None:
+    if not LIBRARY:
+        print("В библеотеке нету книг!!!")
+        return
+    book_number = input("Введите номер(ЦЫФРУ) книги, которую нужно удалить")
+    if not book_number.isdigit():
+        print("Ошибка!Номер книги должен быть числом")
+        return
+    book_number = int(book_number)
+    if book_number <= 0:
+        print("Ошибка! Номер должен быть больше 0")
+        return
+    if book_number > len(LIBRARY):
+        print("Ошибка! Номер СЛИШКОМ большой")
+    idx = book_number - 1
+    book = LIBRARY[idx]
+    print("номер на полке", idx + 1)
+    print("название", book["название"])
+    print("автор", book["автор"])
+    print("год", book["год"])
+
+print("Здраствуйте")
+while True:
+    os.system("cls")
+    key = int(input("Введите: 1 = добавить книгу, 2 = удалить книгу, 3 = показать библиотеку со всеми книгами, 4 = найти книгу по номеру: "))
+    if key == 1:
+        add_book()
+    elif key == 2:
+        delete_book()
+    elif key == 3:
+        show_library()
+    elif key == 4:
+        search_by_number()
+    else:
+        print("введите ЧИСЛО")
+    input("Нажмите ENTER")
