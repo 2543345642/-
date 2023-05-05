cd7b730f51b3eb3baf1c719636b9cd878c32e398
Renatal <58842567+2543345642@users.noreply.github.com>
2023-05-05 17:45:07 +0300

    Add files via upload

---
+++ pong.2.py
@@ -0,0 +1,60 @@
+import pygame
+import sys
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        WHITE = (255, 255, 255)
+        screen_info = pygame.display.Info()
+        self.W = screen_info.current_w
+        self.H = screen_info.current_h
+        self.screen = pygame.display.set_mode(
+            (self.W, self.H),
+            pygame.FULLSCREEN
+        )
+        self.screen_rect = self.screen.get_rect()
+        self.player_1 = Paddle(
+            self.screen_rect.width * 0.1,
+            self.screen_rect.centery,
+            WHITE
+        )
+        self.all_sprites = pygame.sprite.Group() # собираем все спрайты в одну кучу
+        self.all_sprites.add(self.player_1)
+        self.all_sprites.add(self.player_2)
+
+    def main_loop(self):
+        game = True
+        while game:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    game = False
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_ESCAPE]:
+                game = False
+                
+            self.all_sprites.draw(self.screen)
+            pygame.display.flip()
+
+
+class Paddle(pygame.sprite.Sprite):
+    def __init__(
+                self,
+                center_x: int,
+                center_y: int,
+                color: tuple
+    ):
+        super().__init__()
+        self.image = pygame.Surface(
+            (10, 100)
+        )
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = center_x
+        self.rect.centery = center_y
+
+
+game = Game()
+game.main_loop()
+pygame.quit
+sys.exit()
